9ea0a5c79621ab8793932524c60a46cf05546ecc
김세훈 <kks357924@ajou.ac.kr>
2025-06-07 10:25:46 +0900

    bug fix
    fix

--- Assets/Dialogue/Assets/scripts/Dialogue/Icon_Active_Manager.cs
+++ Assets/Dialogue/Assets/scripts/Dialogue/Icon_Active_Manager.cs
@@ -14,18 +14,21 @@ public class Icon_Active_Manager : MonoBehaviour
     {
         for (int i = 0; i < ON_panels.Length; i++)
         {
-
+            if (ON_panels[i].name == "Dialogue_Panel")
+            {
+                if (Dialogue_Manage.Instance.isEndLine()) //끝줄이면 리턴
+                {
+                    return;
+                }
+            }
             ON_panels[i].SetActive(true);
-
         }
     }
     public void Off_Panel()
     {
         for(int i = 0; i < OFF_panels.Length; i++)
-        {
-           
-                OFF_panels[i].SetActive(false);
-            
+        {      
+            OFF_panels[i].SetActive(false);          
         }
     }
 }
