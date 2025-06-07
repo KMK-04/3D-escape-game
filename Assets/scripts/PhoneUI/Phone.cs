3436df58c0c584196b04c33c8a469261f7d2c7d3
Kimmmmmmmmmmmmmmm1 <k31321@ajou.ac.kr>
2025-06-07 17:19:00 +0900

    Phone

--- Assets/scripts/PhoneUI/Phone.cs
+++ Assets/scripts/PhoneUI/Phone.cs
@@ -1,12 +1,13 @@
 using System;
+using SojaExiles;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Phone : MonoBehaviour
-{
+public class Phone : MonoBehaviour {
     public RectTransform phone;
+    public PlayerMovement player;
     public float Desty;
     public ContextData data;
     public float _openy;
@@ -15,33 +16,27 @@ public class Phone : MonoBehaviour
     public bool isOpen = false;
     [HideInInspector]
     public string context;
-    public void TogglePhone()
-    {
+    public void TogglePhone() {
         isOpen = true;
     }
-    public void ClosePhone()
-    {
+    public void ClosePhone() {
         isOpen = false;
     }
 
-    private void Start()
-    {
+    private void Start() {
         context = data.Context;
         //Debug.Log(context);
     }
-    private void Update()
-    {
-        if (isOpen)
-        {
+    private void Update() {
+        if (isOpen) {
             Desty = _openy;
+            player.canMove = false;
         }
-        else
-        {
+        else {
             Desty = _closey;
         }
         float __y = phone.anchoredPosition.y;
-        if (Mathf.Abs(__y - Desty) > 1)
-        {
+        if (Mathf.Abs(__y - Desty) > 1) {
             __y += (Desty - __y) / 10f;
             phone.anchoredPosition = new Vector2(phone.anchoredPosition.x, __y);
         }
