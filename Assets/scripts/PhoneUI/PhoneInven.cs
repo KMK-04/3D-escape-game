2eb7343e301100afb2b861ca89884c99c9f06493
dittoes <ditto2371@ajou.ac.kr>
2025-06-08 03:51:30 +0900

    Revert to 870d793

--- Assets/scripts/PhoneUI/PhoneInven.cs
+++ Assets/scripts/PhoneUI/PhoneInven.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class PhoneInven : MonoBehaviour
-{
+public class PhoneInven : MonoBehaviour {
     private float chatScrollDesty, chatBarDesty, Desty;
 
     public float _chatOpeny, _chatClosey, _barOpeny, _barClosey, Openy, Closey;
     public RectTransform chatScroll, chatBar;
+    ItemData _item;
+    public AIChatManager aiChatManager;
     public bool isOpen = false;
-    public void ToggleInven()
-    {
+    public void ToggleInven() {
         isOpen = !isOpen;
     }
-    public void CloseInven()
-    {
+    public void CloseInven() {
         isOpen = false;
     }
-    private void Update()
-    {
-        if (isOpen)
-        {
+    public void AttachItem() {
+        aiChatManager.SendMessageToGPT("$"+_item.ITEM_Name+"$");
+    }
+
+
+    private void Update() {
+        if (isOpen) {
             chatScrollDesty = _chatOpeny;
             chatBarDesty = _barOpeny;
             Desty = Openy;
         }
-        else
-        {
+        else {
             chatScrollDesty = _chatClosey;
             chatBarDesty = _barClosey;
             Desty = Closey;
@@ -38,20 +39,17 @@ public class PhoneInven : MonoBehaviour
 
         RectTransform rect = GetComponent<RectTransform>();
         float _y = rect.anchoredPosition.y;
-        if (Mathf.Abs(_y - Desty) > 1)
-        {
+        if (Mathf.Abs(_y - Desty) > 1) {
             _y += (Desty - _y) / 10f;
             rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, _y);
         }
         float __y = chatScroll.anchoredPosition.y;
-        if (Mathf.Abs(__y - chatScrollDesty) > 1)
-        {
+        if (Mathf.Abs(__y - chatScrollDesty) > 1) {
             __y += (chatScrollDesty - __y) / 10f;
             chatScroll.anchoredPosition = new Vector2(chatScroll.anchoredPosition.x, __y);
         }
         float ___y = chatBar.anchoredPosition.y;
-        if (Mathf.Abs(___y - chatBarDesty) > 1)
-        {
+        if (Mathf.Abs(___y - chatBarDesty) > 1) {
             ___y += (chatBarDesty - ___y) / 10f;
             chatBar.anchoredPosition = new Vector2(chatBar.anchoredPosition.x, ___y);
         }
